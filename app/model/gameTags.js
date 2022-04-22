6c7a5a8922f42896c4e7ad6323ad7d783eaf3067
yanyanshan <yanyanshan@bigo.sg>
2022-04-22 18:25:16 +0800

    feat：标签表model

---
+++ app/model/gameTags.js
@@ -0,0 +1,39 @@
+'use strict'
+/*  标签库列表  */
+module.exports = app => {
+  const { INTEGER, STRING, DATE } = app.Sequelize
+
+  const GameTags = app.model.define('game_tags', {
+    id: {
+      type: INTEGER(11).UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: '主键',
+    },
+    tagName: {
+      type: STRING(255),
+      allowNull: false,
+      comment: '标签名称',
+      field: 'tag_name',
+      defaultValue: '',
+    },
+    relatedGameCount: {
+      type: INTEGER(11),
+      allowNull: false,
+      comment: '关联游戏数',
+      field: 'related_game_count',
+      defaultValue: 0,
+    },
+    createdAt: {
+      type: DATE,
+      comment: '创建时间',
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DATE,
+      comment: '更新时间',
+      field: 'updated_at',
+    },
+  })
+  return GameTags
+}
